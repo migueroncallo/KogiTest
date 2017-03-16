2f54a7f3edc8936cdf6ad9deea3798acc443897f
mRoncalloNativappsiOS <devops1@nativapps.co>
2017-03-16 13:06:56 -0500

    Initial Commit

---
+++ Kogi Mobile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kogi Mobile
+//
+//  Created by Miguel Roncallo on 3/16/17.
+//  Copyright © 2017 Miguel Roncallo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
